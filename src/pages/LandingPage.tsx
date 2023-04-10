23d646c50eea16d7d11d4250a1ad3609d02069ba
Ibrahim Hafez <cursedlawliet@gmail.com>
2023-04-10 17:33:50 +0300

    implement the filtering functionality and the ui

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,43 +1,51 @@
-import Navbar from "../components/Navbar";
-import Searcher from "../components/Search";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const LandingPage = (): JSX.Element => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+  const handleSearch = () => {
+    navigate(`/search/  ${searchQuery}`);
+  };
   return (
-    <body>
-      <main className="min-h-screen bg-hero bg-no-repeat bg-cover bg-center relative h-1">
-        <div className="absolute bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 w-screen h-screen opacity-20"></div>
-        <div className="h-full relative z-20">
-          <Navbar />
-          {/* hero */}
-          <div className="flex flex-col items-center p-4 ">
-            <h1 className="text-2xl md:text-6xl text-center text-white leading-relaxed">
-              Don't Let Bike Theft Ruin Your Day <br /> Use Our Theft Tracker
-            </h1>
-            <p className="mt-10 text-sm md:text-lg text-white text-center">
-              Bike theft can be a frustrating and upsetting experience,
-              <br />
-              especially if you rely on your bike for transportation or use it
-              <br />
-              for leisure activities. That's why we created our theft tracker
-              <br />
-              web app
-            </p>
-            <form className="w-full max-w-xl mt-10">
-              <div className="bg-white rounded-full flex justify-between items-center p-2">
-                <input
-                  type="text"
-                  className="rounded-full border-none outline-none px-4 min-w"
-                  placeholder="Bike"
-                />
-                <button className="text-white bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-full px-4 py-2">
-                  Search
-                </button>
-              </div>
-            </form>
-          </div>
+    <main className="min-h-screen bg-hero bg-no-repeat bg-cover bg-center relative h-1">
+      <div className="absolute bg-gradient-to-l from-blue-700 via-blue-800 to-gray-900 w-screen h-screen opacity-20"></div>
+      <div className="h-full relative z-20 flex justify-center items-center">
+        {/* hero */}
+        <div className="flex flex-col items-center p-4 ">
+          <h1 className="text-3xl md:text-6xl text-center text-white leading-relaxed">
+            Don't Let Bike Theft Ruin Your Day <br /> Use Our Theft Tracker
+          </h1>
+          <p className="mt-10 text-sm md:text-lg text-white text-center">
+            Bike theft can be a frustrating and upsetting experience,
+            <br />
+            especially if you rely on your bike for transportation or use it
+            <br />
+            for leisure activities. That's why we created our theft tracker
+            <br />
+            web app
+          </p>
+          <form className="w-full max-w-xl mt-10">
+            <div className="bg-white rounded-full flex justify-between items-center p-2">
+              <input
+                type="text"
+                className="rounded-full border-none outline-none px-4 min-w"
+                placeholder="Bike"
+                value={searchQuery}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  setSearchQuery(event.target.value)
+                }
+              />
+              <button
+                onClick={handleSearch}
+                className="text-white bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-full px-4 py-2"
+              >
+                Search
+              </button>
+            </div>
+          </form>
         </div>
-      </main>
-    </body>
+      </div>
+    </main>
   );
 };
 
--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,99 +1,141 @@
-import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import FilterBar from "../components/FilterBar";
-import Pagination from "../components/Pagination";
-import ResultCard from "../components/ResultCard";
-import Searcher from "../components/Search";
-import { BikeTheft, data } from "../dummy";
+import React, { useState, useEffect } from "react";
+import { ImLocation } from "react-icons/im";
+import { AiFillFilter } from "react-icons/ai";
+import { BsCalendarDate } from "react-icons/bs";
+import { data } from "../dummy";
+import type { BikeTheft } from "../dummy";
+import { useParams } from "react-router-dom";
 
-export interface FilterValues {
+type Props = {};
+type formData = {
+  from: string;
+  to: string;
   location: string;
-  fromDate: string;
-  toDate: string;
-}
+};
 
-const ResultsPage = () => {
-  const { query = "" } = useParams();
-  const navigate = useNavigate();
-  const [filterValues, setFilterValues] = useState<FilterValues>({
+const ResultsPage = (props: Props) => {
+  const { query } = useParams();
+
+  const [content, setContent] = useState<BikeTheft[]>();
+  const [searchFilteredData, setSearchFilteredData] = useState<BikeTheft[]>(
+    data.filter((e) => e.caseTitle.split(" ").includes(query!))
+  );
+  const [formData, setFormData] = useState<formData>({
+    from: "",
+    to: "",
     location: "",
-    fromDate: "",
-    toDate: "",
   });
 
-  const handleFilterChange = (values: FilterValues) => {
-    setFilterValues(values);
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
   };
-
-  const filteredData = data.filter((item: BikeTheft) => {
-    const { location, fromDate, toDate } = filterValues;
-    const isLocationMatch =
-      location === "" ||
-      item.location.toLowerCase().includes(location.toLowerCase());
-    const isDateMatch =
-      (fromDate === "" || item.theftDate >= fromDate) &&
-      (toDate === "" || item.theftDate <= toDate);
-    const isQueryMatch =
-      query === "" ||
-      item.caseTitle.toLowerCase().includes(query.toLowerCase());
-
-    return isLocationMatch && isDateMatch && isQueryMatch;
-  });
-
-  const itemsPerPage = 5;
-  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentData = filteredData.slice(startIndex, endIndex);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
+  console.log("content", content);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(searchFilteredData);
+    if (Object.values(formData).every((e) => e === "")) {
+      setContent(searchFilteredData);
+    } else {
+      const filteredData =
+        searchFilteredData &&
+        searchFilteredData
+          .filter((bike) =>
+            bike.location.split(" ").includes(formData.location)
+          )
+          .filter((bike) => {
+            if (formData.from) {
+              return new Date(bike.theftDate) >= new Date(formData.from);
+            }
+            return bike;
+          })
+          .filter((bike) => {
+            if (formData.to) {
+              return new Date(bike.theftDate) <= new Date(formData.to);
+            }
+            return bike;
+          });
+      setContent(filteredData);
+    }
   };
-
-  console.log(currentData);
   return (
-    <body className="bg-gray-100 min-h-screen px-36">
-      <div className="flex">
-        <div className="flex flex-col p-12">
-          <div className="w-5/6">
-            <button
-              className="p-2.5 mb-2 text-sm font-medium text-white bg-purple-700 rounded-lg border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
-              onClick={() => navigate("/")}
-            >
-              Home
-            </button>
-            <Searcher initialValue={query} />
-            <FilterBar onChange={handleFilterChange} />
-            {currentData.length ? (
-              <>
-                {currentData.map((item, key) => (
-                  <ResultCard
-                    key={key}
-                    name={item.caseTitle}
-                    description={item.description}
-                    area={item.location}
-                    date={item.reportDate}
-                    lat={item.latitude}
-                    lon={item.longitude}
-                  />
-                ))}
-              </>
-            ) : (
-              <h1 className="mt-12">No results Found</h1>
-            )}
+    <main className="min-h-screen h-1 bg-[#f1f2f8] p-2">
+      <div className="w-full h-[35%] mb-4 bg-red-500 rounded-lg overflow-hidden">
+        <img
+          src="https://images.unsplash.com/photo-1532298229144-0ec0c57515c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80"
+          alt="bike"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="bg-white w-[90%] mt-[-80px] md:mt-[-50px] mx-auto relative z-10 md:rounded-full p-2 flex items-center justify-between flex-wrap rounded-lg md:gap-[1%] gap-[2%] gap-y-3 ">
+          <div className="flex items-center border-r-[1px] w-[48%] md:w-[24%] border-r-slate-200 ">
+            <ImLocation className="text-slate-400 mr-2 w-10" />
+            <div className="flex flex-col">
+              <label htmlFor="location" className="capitalize text-gray-400">
+                WHERE
+              </label>
+              <input
+                name="location"
+                id="location"
+                type="text"
+                className="my-input focus:outline-none w-full"
+                onChange={handleFormChange}
+              />
+            </div>
           </div>
-          <div className="w-5/6">
-            <Pagination
-              currentPage={currentPage}
-              totalPages={totalPages}
-              onPageChange={handlePageChange}
-            />
+          <div className="flex items-center md:border-r-[1px] w-[48%] md:w-[24%] md:border-r-slate-200 ">
+            <BsCalendarDate className="text-slate-400 mr-2 w-10" />
+            <div className="flex flex-col">
+              <label htmlFor="location" className="capitalize text-gray-400">
+                FROM
+              </label>
+              <input
+                name="from"
+                max={formData.to}
+                id="location"
+                type="date"
+                className="focus:outline-none w-full"
+                onChange={handleFormChange}
+              />
+            </div>
+          </div>
+          <div className="flex items-center border-r-[1px] w-[48%] md:w-[24%] border-r-slate-200 ">
+            <BsCalendarDate className="text-slate-400 mr-2 w-10" />
+            <div className="flex flex-col">
+              <label htmlFor="location" className="capitalize text-gray-400">
+                TO
+              </label>
+              <input
+                name="to"
+                min={formData.from}
+                id="location"
+                type="date"
+                className="focus:outline-none w-full"
+                onChange={handleFormChange}
+              />
+            </div>
+          </div>
+          <div className="w-[48%] md:w-[24%]">
+            <button
+              type="submit"
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium w-full p-3 md:rounded-br-full md:rounded-tr-full transition-color duration-200"
+            >
+              <span className="flex items-center justify-center">
+                <AiFillFilter className="mr-2" /> Filter
+              </span>
+            </button>
           </div>
         </div>
-      </div>
-    </body>
+      </form>
+      {content && content.length === 0 ? (
+        <p>NO CONTENT FOUND</p>
+      ) : (
+        <p>HERE IS THE BIKE THEFT</p>
+      )}
+    </main>
   );
 };
 
